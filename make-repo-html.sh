3b1ea4775492608ca2006e691ea7948731a1e2b0
Wouter Wijsman <wouter@gridscale.io>
2024-06-20 18:17:50 +0200

    Comment out gzip command for now

--- make-repo-html.sh
+++ make-repo-html.sh
@@ -27,9 +27,9 @@ for PSPBUILD in $(find . -name "PSPBUILD" | sort); do
 		# Get file size info
 		FILENAME="repo/${DOWNLOAD_URL}"
 		if [ -f "${FILENAME}" ]; then
-			//GZSTATS="$(gzip -l $FILENAME | tail -n 1 | tr -s ' ')"
-			//PKGSIZE=$(echo $GZSTATS | cut -d ' ' -f 1 | numfmt --to=iec --suffix=B --format="%.2f")
-  			//INSTSIZE=$(echo $GZSTATS | cut -d ' ' -f 2 | numfmt --to=iec --suffix=B --format="%.2f")
+			# GZSTATS="$(gzip -l $FILENAME | tail -n 1 | tr -s ' ')"
+			# PKGSIZE=$(echo $GZSTATS | cut -d ' ' -f 1 | numfmt --to=iec --suffix=B --format="%.2f")
+  			# INSTSIZE=$(echo $GZSTATS | cut -d ' ' -f 2 | numfmt --to=iec --suffix=B --format="%.2f")
 		fi
 
 		# List dependencies
