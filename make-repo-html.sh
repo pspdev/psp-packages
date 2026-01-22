7e3d48bb6180bd1809b4b4f2217299bebc56ec1c
Wouter Wijsman <wouter@gridscale.io>
2026-01-22 14:41:51 +0100

    Add source links

--- make-repo-html.sh
+++ make-repo-html.sh
@@ -18,6 +18,29 @@ PACKAGES="$(find . -maxdepth 2 -name "${PKGBUILD_NAME}" | sort)"
 PACKAGE_COUNT="$(echo ${PACKAGES} | wc -w)"
 INDEX_TABLE_CONTENT=""
 
+function createSourceLink() {
+	src="${1}"
+	pkgname="${2}"
+	if [[ ${1} == git+* ]]; then
+		BASE_URL="$(echo $1 | cut -d'+' -f2- | rev | cut -d'.' -f2- | rev)" 
+		if [[ ${1} == *.git#commit=* ]]; then
+			COMMIT="$(echo $1 | cut -d'=' -f2)"
+			URL="${BASE_URL}/tree/${COMMIT}"
+			echo "<a href=\"${URL}\">${BASE_URL}</a>"
+		elif [[ ${1} == *.git#branch=* ]]; then
+			BRANCH="$(echo $1 | cut -d'=' -f2)"
+			URL="${BASE_URL}/tree/${BRANCH}"
+			echo "<a href=\"${URL}\">${BASE_URL}</a>"
+		else
+			echo "<a href=\"${BASE_URL}\">${BASE_URL}</a>"
+		fi
+	elif [[ ${1} == http?://* ]] || [[ ${1} == ftp?://* ]];; then
+			echo "<a href=\"${1}\">${1}</a>"
+	else
+		echo "<a href=\"https://github.com/${GITHUB_REPOSITORY}/blob/master/${pkgname}/${1}\">${1}</a>"
+	fi
+}
+
 # Build the html pages
 for PKGBUILD in ${PACKAGES}; do
   		# Make sure optional variables are from current PKGBUILD
@@ -50,6 +73,17 @@ for PKGBUILD in ${PACKAGES}; do
 				DEPS="${DEPS}<li><a href=\"${dep}.html\">${dep}</a></li>"
 			done
 			DEPS="${DEPS}</ul>"
+		fi
+
+		# List dependencies
+		if [ ! -n "${source[*]}" ]; then
+			SOURCES="No sources"
+		else
+			SOURCES="<ul>"
+			for src in "${source[@]}"; do
+				SOURCES="${SOURCES}<li>$(createSourceLink "${src}" "${pkgname}")</li>"
+			done
+			SOURCES="${SOURCES}</ul>"
 		fi
 
 		# List content of files
